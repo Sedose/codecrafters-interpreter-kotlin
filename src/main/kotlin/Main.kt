eaebb529ac1495c9bcb50c2fa84704061917b738
Edhar Avuzi <Edhar.EzenvaAvuzi@kingfisher.com>
2025-03-01 06:15:03 +0200

    Scanning: Parentheses

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,28 +2,34 @@ import java.io.File
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.err.println("Logs from your program will appear here!")
+  System.err.println("Logs from your program will appear here!")
 
-    if (args.size < 2) {
-        System.err.println("Usage: ./your_program.sh tokenize <filename>")
-        exitProcess(1)
-    }
+  if (args.size < 2) {
+    System.err.println("Usage: ./your_program.sh tokenize <filename>")
+    exitProcess(1)
+  }
 
-    val command = args[0]
-    val filename = args[1]
+  val (command, filename) = args
 
-    if (command != "tokenize") {
-        System.err.println("Unknown command: ${command}")
-        exitProcess(1)
-    }
+  if (command != "tokenize") {
+    System.err.println("Unknown command: $command")
+    exitProcess(1)
+  }
 
-    val fileContents = File(filename).readText()
+  val fileContents = File(filename).readText()
 
-    // Uncomment this block to pass the first stage
-     if (fileContents.isNotEmpty()) {
-         throw NotImplementedError("Scanner not implemented")
-     } else {
-         println("EOF  null") // Placeholder, remove this line when implementing the scanner
-     }
+  val tokenToType =
+    mapOf(
+      '(' to "LEFT_PAREN",
+      ')' to "RIGHT_PAREN",
+    )
+
+  if (fileContents.isNotEmpty()) {
+    fileContents.toCharArray()
+      .joinToString(separator = "\n") { token ->
+        val tokenType = tokenToType[token] ?: throw RuntimeException("Unknown token: $token")
+        "$tokenType $token null"
+      }.let(::println)
+  }
+  println("EOF  null")
 }
