817e3a0ed610fac3abb47f45f6526a64baa3ffd0
Edhar Avuzi <Edhar.EzenvaAvuzi@kingfisher.com>
2025-03-01 21:25:15 +0200

    Setup testing

--- src/main/kotlin/io/codecrafters/Tokenizer.kt
+++ src/main/kotlin/io/codecrafters/Tokenizer.kt
@@ -149,22 +149,23 @@ class Tokenizer : KoinComponent {
       '>' to TokenType.GREATER
     )
 
-  private val reservedWords = mapOf(
-    "and" to TokenType.AND,
-    "class" to TokenType.CLASS,
-    "else" to TokenType.ELSE,
-    "false" to TokenType.FALSE,
-    "for" to TokenType.FOR,
-    "fun" to TokenType.FUN,
-    "if" to TokenType.IF,
-    "nil" to TokenType.NIL,
-    "or" to TokenType.OR,
-    "print" to TokenType.PRINT,
-    "return" to TokenType.RETURN,
-    "super" to TokenType.SUPER,
-    "this" to TokenType.THIS,
-    "true" to TokenType.TRUE,
-    "var" to TokenType.VAR,
-    "while" to TokenType.WHILE,
-  )
+  private val reservedWords =
+    mapOf(
+      "and" to TokenType.AND,
+      "class" to TokenType.CLASS,
+      "else" to TokenType.ELSE,
+      "false" to TokenType.FALSE,
+      "for" to TokenType.FOR,
+      "fun" to TokenType.FUN,
+      "if" to TokenType.IF,
+      "nil" to TokenType.NIL,
+      "or" to TokenType.OR,
+      "print" to TokenType.PRINT,
+      "return" to TokenType.RETURN,
+      "super" to TokenType.SUPER,
+      "this" to TokenType.THIS,
+      "true" to TokenType.TRUE,
+      "var" to TokenType.VAR,
+      "while" to TokenType.WHILE,
+    )
 }
