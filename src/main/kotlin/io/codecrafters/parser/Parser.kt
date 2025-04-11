fdfb3d3c65673571b266bfda41425f7316965a04
Edhar Avuzi <Edhar.EzenvaAvuzi@kingfisher.com>
2025-04-11 08:19:55 +0300

    refactor: unify binary expression parsing to eliminate duplication

--- src/main/kotlin/io/codecrafters/parser/Parser.kt
+++ src/main/kotlin/io/codecrafters/parser/Parser.kt
@@ -12,6 +12,7 @@ import io.codecrafters.model.TokenType
 class Parser(
   private val tokens: List<Token>,
 ) {
+  private val unaryTokenTypes = setOf(TokenType.BANG, TokenType.MINUS)
   private var currentTokenIndex = 0
 
   fun parse(): Either<ParseError, Expr> =
@@ -21,55 +22,44 @@ class Parser(
 
   private fun parseExpression(raise: Raise<ParseError>): Expr = parseEquality(raise)
 
-  private fun parseEquality(raise: Raise<ParseError>): Expr {
-    var expression = parseComparison(raise)
+  private fun parseEquality(raise: Raise<ParseError>): Expr =
+    parseBinaryRightAssociative(
+      raise,
+      ::parseComparison,
+      setOf(TokenType.EQUAL_EQUAL, TokenType.BANG_EQUAL),
+    )
 
-    while (match(TokenType.EQUAL_EQUAL, TokenType.BANG_EQUAL)) {
+  private fun parseComparison(raise: Raise<ParseError>): Expr =
+    parseBinaryRightAssociative(
+      raise,
+      ::parseAdditive,
+      setOf(TokenType.GREATER, TokenType.GREATER_EQUAL, TokenType.LESS, TokenType.LESS_EQUAL),
+    )
+
+  private fun parseAdditive(raise: Raise<ParseError>): Expr =
+    parseBinaryRightAssociative(
+      raise,
+      ::parseMultiplicative,
+      setOf(TokenType.PLUS, TokenType.MINUS),
+    )
+
+  private fun parseMultiplicative(raise: Raise<ParseError>): Expr =
+    parseBinaryRightAssociative(
+      raise,
+      ::parseUnary,
+      setOf(TokenType.STAR, TokenType.SLASH),
+    )
+
+  private inline fun parseBinaryRightAssociative(
+    raise: Raise<ParseError>,
+    parseNext: (Raise<ParseError>) -> Expr,
+    operators: Set<TokenType>,
+  ): Expr {
+    var expression = parseNext(raise)
+
+    while (match(operators)) {
       val operator = previousToken()
-      val right = parseComparison(raise)
-      expression = Expr.Binary(expression, operator, right)
-    }
-
-    return expression
-  }
-
-  private fun parseComparison(raise: Raise<ParseError>): Expr {
-    var expression = parseAdditive(raise)
-
-    while (
-      match(
-        TokenType.GREATER,
-        TokenType.GREATER_EQUAL,
-        TokenType.LESS,
-        TokenType.LESS_EQUAL,
-      )
-    ) {
-      val operator = previousToken()
-      val right = parseAdditive(raise)
-      expression = Expr.Binary(expression, operator, right)
-    }
-
-    return expression
-  }
-
-  private fun parseAdditive(raise: Raise<ParseError>): Expr {
-    var expression = parseMultiplicative(raise)
-
-    while (match(TokenType.PLUS, TokenType.MINUS)) {
-      val operator = previousToken()
-      val right = parseMultiplicative(raise)
-      expression = Expr.Binary(expression, operator, right)
-    }
-
-    return expression
-  }
-
-  private fun parseMultiplicative(raise: Raise<ParseError>): Expr {
-    var expression = parseUnary(raise)
-
-    while (match(TokenType.STAR, TokenType.SLASH)) {
-      val operator = previousToken()
-      val right = parseUnary(raise)
+      val right = parseNext(raise)
       expression = Expr.Binary(expression, operator, right)
     }
 
@@ -77,7 +67,7 @@ class Parser(
   }
 
   private fun parseUnary(raise: Raise<ParseError>): Expr =
-    if (match(TokenType.BANG, TokenType.MINUS)) {
+    if (match(unaryTokenTypes)) {
       val operator = previousToken()
       val right = parseUnary(raise)
       Expr.Unary(operator, right)
@@ -133,7 +123,7 @@ class Parser(
 
   private fun peek(): Token = tokens[currentTokenIndex]
 
-  private fun match(vararg types: TokenType): Boolean {
+  private fun match(types: Set<TokenType>): Boolean {
     for (type in types) {
       if (check(type)) {
         advanceToken()
