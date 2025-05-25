6f10f79dbdde25550ae20a6b212e60a8f53e8249
Edhar Avuzi <Edhar.EzenvaAvuzi@kingfisher.com>
2025-05-26 01:43:56 +0300

    Fix

--- your_program.sh
+++ your_program.sh
@@ -21,4 +21,4 @@ set -e # Exit early if any commands fail
 #
 # - Edit this to change how your program runs locally
 # - Edit .codecrafters/run.sh to change how your program runs remotely
-exec java -jar /tmp/codecrafters-build-dir/build-your-own-interpreter.jar "$@"
+exec java -jar /tmp/codecrafters-build-dir/build-your-own-interpreter-1.0.jar "$@"
